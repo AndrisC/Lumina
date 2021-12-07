14644467452f5262ae0e1a10f346cbb5fa8daf9c
AndrisC <csipszerandras@gmail.com>
2021-12-07 17:28:00 +0100

    Starry sky background

---
+++ components/lua-navbar.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="component-wrapper">
+    <div class="title-wrapper">
+      <h3>Lumina</h3>
+    </div>
+
+    <div class="menu-wrapper">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,9 @@
     <div class="default-layout">
       <div class="content" id="content">
         <Nuxt />
+        <div id='stars'></div>
+        <div id='stars2'></div>
+        <div id="stars3"></div>
       </div>
     </div>
 
@@ -19,32 +22,109 @@ export default {
 }
 </script>
 
-<style lang="scss">
-html {
-  font-family: Inter;
-  font-size: 18px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<style lang="sass">
+html
+  font-family: Inter
+  font-size: 18px
+  word-spacing: 1px
+  -ms-text-size-adjust: 100%
+  -webkit-text-size-adjust: 100%
+  -moz-osx-font-smoothing: grayscale
+  -webkit-font-smoothing: antialiased
+  box-sizing: border-box
+
 *,
 *:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-.content {
-  width: 100%;
-  overflow-y: auto;
-  background: #fff;
-}
-.default-layout {
-  min-height: calc(100vh - 64px);
-  width: 100%;
-  display: flex;
-  background: #fff;
-}
+*:after
+  box-sizing: border-box
+  margin: 0
+
+.content
+  width: 100%
+  overflow-y: auto
+  background: radial-gradient(ellipse at bottom, #1B2735 0%, #090A0F 100%)
+  overflow: hidden
+
+
+
+@use compass
+// n is number of stars required
+@function multiple-box-shadow ($n)
+  $value: '#{random(2000)}px #{random(2000)}px #a8a8a8'
+  @for $i from 2 through $n
+    $value: '#{$value} , #{random(2000)}px #{random(2000)}px #8f8f8f'
+
+  @return unquote($value)
+
+$shadows-small:  multiple-box-shadow(700)
+$shadows-medium: multiple-box-shadow(200)
+$shadows-big:    multiple-box-shadow(100)
+
+#stars
+  width: 1px
+  height: 1px
+  background: transparent
+  box-shadow: $shadows-small
+  animation			: animStar 100s linear infinite
+
+  &:after
+    content: " "
+    position: absolute
+    top: 2000px
+    width: 1px
+    height: 1px
+    background: transparent
+    box-shadow: $shadows-small
+
+#stars2
+  width: 2px
+  height: 2px
+  background: transparent
+  box-shadow: $shadows-medium
+  animation			: animStar 250s linear infinite
+
+  &:after
+    content: " "
+    position: absolute
+    top: 2000px
+    width: 2px
+    height: 2px
+    background: transparent
+    box-shadow: $shadows-medium
+
+#stars3
+  width: 3px
+  height: 3px
+  background: transparent
+  box-shadow: $shadows-big
+  animation			: animStar 350s linear infinite
+
+  &:after
+    content: " "
+    position: absolute
+    top: 2000px
+    width: 3px
+    height: 3px
+    background: transparent
+    box-shadow: $shadows-big
+
+@keyframes animStar
+  from
+    transform: translateY(0px)
+  to
+    transform: translateY(-2000px)
+
+
+#stars
+  width: 1px
+  height: 1px
+  background: transparent
+  box-shadow: $shadows-small
+
+.default-layout
+  min-height: 100vh
+  width: 100%
+  display: flex
+  background: #fff
+
 </style>
