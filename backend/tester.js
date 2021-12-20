c5846a4ee324e1142417d934d0b674e772a5f5e9
Nemjatekosm <upkh4ck3r@gmail.com>
2021-12-20 12:20:06 +0100

    planetgen changes in the tester.js file

--- backend/tester.js
+++ backend/tester.js
@@ -1,16 +1,16 @@
 import namegen from "../backend/namegen.js";
 import solarsysgen from "../backend/solarsysgen";
+import animalgen from "./animalgen.js";
+import civilizationgen from "./civilizationgen.js";
 
 export default {
     planetgen: function (number, star) {
-       let allPlanet = [];
+        let allPlanet = [];
 
         var planetNames = namegen.planetnamegen(number);
 
-        console.warn(planetNames);
-
         // console.warn(planetNames);
-        for (let i = 0; i <= number-1; i++) {
+        for (let i = 0; i <= number - 1; i++) {
 
 
             //GENERATE PLACEHOLDERS
@@ -26,79 +26,114 @@ export default {
 
 
             //Generate radius between 5200 - 200 km
-            let newRadius = Math.floor(Math.random() * (5200 - 200) + 200);
-            
-
-            //Generate the amount of gas
+            let newRadius = Math.floor(Math.random() * (3200 - 400) + 400);
 
 
-            
-            //generate type
-            var planetTypes = ["Ice", "Water", "Gas", "Earth-like", "Toxic"]
-            let newPlanetType = planetTypes[Math.floor(Math.random() * planetTypes.length)];
+            //Calculate mass *10^24
+            let newMass = Math.floor(newRadius * newRadius * (1000 / newRadius) / 100) / 1000;
 
+            //Calculate gravity g = G*M/r^2
+            let newGravity = newMass / newRadius * newRadius;
+            console.warn(newGravity);
 
-            //Generate mass. You have to multply it by 100 000 000 000 000 000 000 (in tons)
-            let newMass = newRadius  * 9.375;
-            
-
-            //Earth's radius and mass value
-            var compEarthRadius = 637;
-            var compEarthMass = 6000;
-
-            
-
-            if (newPlanetType == "Gas") {
-                newMass = newMass / 3;
-            }
-
-            console.log("Compared to earth Radius: " + newRadius / compEarthRadius);
-            console.log("Compared to earth Mass: " + newMass / compEarthMass);
-            console.warn(planetNames[i] + " Mass: " + newMass + " Radius: " + newRadius);
 
 
             //Generate moons max 8
             let newMoons = Math.floor(Math.random() * 8);
 
-
-           
-
-
-            //Generate animals if needed
-
-
-            //Generate vegetation if needed
+            //Generate moon names
+            let newMoonNames = namegen.planetnamegen(newMoons);
 
 
             //Calculate the distance from the star between 1012 - 10 km. Must multiply with 1 000 000
             let newDistance = Math.floor(Math.random() * 10020) / 10 + 10;
 
+
+            //Calculate the length of a year in days
+            //let newLengthOfAYear = newDistance / 93 * 365;
+
+            //Calculate the lenght of a day (in hours) 4 - 400
+            let newLengthOfADay = Math.floor(Math.random() * (440 - 4) + 4); 
+
+
+            //Generate gases
+
+            var newHydrogen = Math.floor(Math.random() * (100 - 0));
+            var newHelium = Math.floor(Math.random() * (100 - 0));
+            var newNitrogen = Math.floor(Math.random() * (100 - 0));
+            var newOxygen = Math.floor(Math.random() * (100 - 0));
+            var newWater = Math.floor(Math.random() * (100 - 0));
+            var newOther = Math.floor(Math.random() * (100 - 0));
+
+            let gasesSum = newHydrogen + newHelium + newNitrogen + newOxygen + newWater + newOther;
+
+            newHydrogen = 100 / gasesSum * newHydrogen;
+            newHelium = 100 / gasesSum * newHelium;
+            newNitrogen = 100 / gasesSum * newNitrogen;
+            newOxygen = 100 / gasesSum * newOxygen;
+            newWater = 100 / gasesSum * newWater;
+            newOther = 100 / gasesSum * newOther;
+
+            gasesSum = gasesSum = newHydrogen + newHelium + newNitrogen + newOxygen + newWater + newOther;
+
+            //generate type
+
+            let newPlanetType = "Toxic"
+
+
+            if (newDistance >= 60 && newDistance <= 200) {
+                newPlanetType = "Water";
+                if (newNitrogen >= 25 && newOxygen >= 6) {
+                    newPlanetType = "Earth-like";
+                }
+            } else if (newHydrogen >= 25) {
+                newPlanetType = "Gas";
+            } else if (newDistance >= 180 && newWater >= 25) {
+                newPlanetType = "Ice";
+            }
+
             //Generate surface water if needed
+            var newSurfaceWater;
+            if (newPlanetType == "Water") {
+                newSurfaceWater = 100;
+            } else if (newPlanetType == "Earth-like") {
+                newSurfaceWater = Math.floor(Math.random() * (100 - 10));
+            } else {
+                newSurfaceWater = 0;
+            }
+
+            var newHydrogen = Math.floor(Math.random() * (100 - 0));
 
 
-            
+            //Generate animals if needed
+
+            //Ice-type animals
+
+            //var animalNames = animalgen.animalgen(0, 1);
+            //console.warn(animalNames);
+
+
+            //Water-type animals
 
 
             //Generate civilization if needed
+            var newCivilization = civilizationgen.civilizationgen(newPlanetType);
 
-
-            //Calculate the lenght of a day
-
-
-            //Calculate the length of a year
+            let newLengthOfAYear = (Math.PI * 2) * Math.sqrt(newRadius*newRadius*newRadius / newGravity);
+            console.warn("Distance from the sun: " + newDistance + "  Length of a year: " + newLengthOfADay);
 
 
             //Generate planetary ring
 
 
             //Liveable?
+            var newLivable = "";
+            
 
             //Generate seed
             let newSeed = Math.floor(Math.random() * (999999 - 1) + 1)
 
-            if (star) {
 
-            }
 
             var planetInfo = {
                 seed: newSeed,
@@ -106,54 +141,82 @@ export default {
                 name: newName,
                 orbitedStar: star,
                 radius: newRadius,
-                mass: "",
+                mass: newMass,
                 moons: newMoons,
+                moonNames: newMoonNames,
                 type: newPlanetType,
-                animals: PLACEHOLDER,
-                vegetation: PLACEHOLDER,
+                animals: {
+                    name: "",
+                    description: "",
+                },
                 liveable: PLACEHOLDER,
                 distanceFromStar: newDistance,
-                surfaceWater:PLACEHOLDER,
-                gas: {
-                    nitrogen: "",
-                    oxygen: "",
-                    water: "",
-                    argon: "",
-                    carbon_dioxide: "",
-                    neon: "",
-                    methane: "",
-                    ozone: "",
+                surfaceWater: newSurfaceWater,
+                gas: [
+                    { name: 'nitrogen', value: newNitrogen },
+                    { name: 'oxygen', value: newOxygen },
+                    { name: 'water', value: newWater },
+                    { name: 'hydrogen', value: newHydrogen },
+                    { name: 'helium', value: newHelium },
+                    { name: 'other', value: newOther }
+                ],
+                civilization: {
+                    name: newCivilization[0],
+                    race: newCivilization[1],
+                    description: newCivilization[2],
+                    kardashev_scale: newCivilization[3],
                 },
-                civilization: PLACEHOLDER,
-                lengthOfDay: PLACEHOLDER,
-                lengthOfYear: PLACEHOLDER,
+                lengthOfDay: newLengthOfADay,
+                lengthOfYear: newLengthOfAYear,
                 planetaryRing: PLACEHOLDER
             };
-
+            console.warn(planetInfo);
             allPlanet.push(planetInfo);
 
         }
         return allPlanet;
     },
     stargen() {
-      //Generate seed
-      let newSeed = Math.floor(Math.random() * (999999 - 1) + 1)
-      //Generate id
-      var newId = 0;
-      //Generate star name
-      var starName = namegen.planetnamegen(1);
-      //Generate radius between 5200 - 1000 km
-      let newRadius = Math.floor(Math.random() * (5200 - 1000) + 1000);
+        //Generate seed
+        let newSeed = Math.floor(Math.random() * (999999 - 1) + 1)
+        //Generate id
+        var newId = 0;
+        //Generate star name
+        var starName = namegen.planetnamegen(1);
+        //Generate radius between 7200 - 4000 km
+        let newRadius = Math.floor(Math.random() * (7020 - 4000) + 4000);
+        //Generate surface temperature
+        let newTemperature = Math.floor(Math.random() * (36000 - 2400) + 4000);
+        //Calculate class from temperature
+        var newClass = "Unknow class";
+        if (newTemperature <= 3700) {
+            newClass = "Class M"; 0
+        } else if (newTemperature >= 3700 && newTemperature <= 5200) {
+            newClass = "Class K"
+        } else if (newTemperature >= 5200 && newTemperature <= 6000) {
+            newClass = "Class G"
+        } else if (newTemperature >= 6000 && newTemperature <= 7500) {
+            newClass = "Class F"
+        } else if (newTemperature >= 7500 && newTemperature <= 10000) {
+            newClass = "Class A"
+        } else if (newTemperature >= 10000 && newTemperature <= 30000) {
+            newClass = "Class B"
+        } else if (newTemperature >= 30000) {
+            newClass = "Class O"
+        }
 
-      var star = {
-        seed: newSeed,
-        _id: newId,
-        name: starName[0],
-        radius: newRadius,
-        mass: "",
-        surfaceTemperature: "",
-        type: "Star",
-      };
-      return star;
+
+        var star = {
+            seed: newSeed,
+            _id: newId,
+            name: starName[0],
+            radius: newRadius,
+            mass: "",
+            surfaceTemperature: newTemperature,
+            class: newClass,
+            type: "Star",
+        };
+        console.warn(star);
+        return star;
     }
 }
